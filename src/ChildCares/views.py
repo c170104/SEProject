9cce13cb936addac8bf7a4a3d0af48389174c2f6
ben <benben-x3@hotmail.com>
2018-03-12 13:06:25 +0800

    Added - fully functional pagination Added - 'localhost' to ALLOWED HOSTS

--- src/ChildCares/views.py
+++ src/ChildCares/views.py
@@ -4,7 +4,7 @@ import json
 import sys
 
 # Create your views here.
-url = "https://data.gov.sg/api/action/datastore_search?resource_id=0c14ceec-da1b-43c6-92fc-e82d7219840b&limit=1"
+url = "https://data.gov.sg/api/action/datastore_search?resource_id=0c14ceec-da1b-43c6-92fc-e82d7219840b"
 
 def index(request):
     # data API
@@ -24,10 +24,10 @@ def index(request):
     else:
         itemStart = 0
         itemEnd = 20
-
+    pages = int(len(results) / 20)
     results = results[itemStart:itemEnd]
     
-    return render(request, 'ChildCares/index.html', {'active_page': 'childcares','content': results})
+    return render(request, 'ChildCares/index.html', {'active_page': 'childcares','content': results, 'page_number': int(pageNumber), 'pages': range(pages)})
 
 def filter(request):
     return render(request, 'ChildCares/index.html', {'active_page': 'childcares', 'content': ["FILTERED DATA HERE"]})
