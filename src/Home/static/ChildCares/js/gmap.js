f4afea80d6bb5edd771eca8299faac6ae0037fae
ben <benben-x3@hotmail.com>
2018-03-13 17:41:16 +0800

    Added - Google Maps to moreinfo

---
+++ src/Home/static/ChildCares/js/gmap.js
@@ -0,0 +1,29 @@
+var map;
+
+function initMap(latLng) {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: latLng,
+        zoom: 18
+    });
+    var marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+    });
+}
+
+function getLatLng(address) {
+    var geocoder_url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyCZYP8lJtnTmatEBazCSE3Tf10KmfnvcBw&address=" + address; 
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", encodeURI(geocoder_url), false);
+    xhttp.send();
+    var response = JSON.parse(xhttp.responseText);
+    // console.log(response)
+    return response['results']['0']['geometry']['location'];
+}
+
+window.onload = function()  {
+    var address = document.getElementById('address').innerHTML;
+    var latLng;
+    latLng = (getLatLng(address));
+    initMap(latLng);
+}
