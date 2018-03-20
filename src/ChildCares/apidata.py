ebed5728f171e76b1605ac19b6e587f7fa8c7826
ben <benben-x3@hotmail.com>
2018-03-20 22:10:09 +0800

    Added - Sort By Preference

--- src/ChildCares/apidata.py
+++ src/ChildCares/apidata.py
@@ -4,11 +4,16 @@ import json
 urlwCost = "https://data.gov.sg/api/action/datastore_search?resource_id=0c14ceec-da1b-43c6-92fc-e82d7219840b"
 urlwInfo = "https://data.gov.sg/api/action/datastore_search?resource_id=4fc3fd79-64f2-4027-8d5b-ce0d7c279646"
 
-def main(query):
+def main(query, sort):
     dataWithCost = apiQuery(urlwCost + "&q=" + query)
     dataWithInfo = apiQuery(urlwInfo + "&q=" + query)
 
     newList = combineList(dataWithCost, dataWithInfo)
+    newList = removeEmptyData(newList)
+
+    if sort != '':
+        newList = sortData(newList, sort)
+
     return newList
 
 
@@ -27,3 +32,30 @@ def combineList(list1, list2):
                 c.update(i)
                 c['centre_code'] = c['\ufeffcentre_code']
     return list1
+
+def removeEmptyData(list1):
+    for c in list1[:]:
+        if not 'centre_code' in c:
+            list1.remove(c)
+    return list1
+
+def sortData(list1, preference):
+    if preference == 'cc':
+        preference = 'centre_code'
+    elif preference == 'name':
+        preference = 'centre_name'
+    elif preference == 'fee':
+        preference = 'fees'
+    elif preference == 'programme':
+        preference = 'programmes_offered'
+    elif preference == 'time':
+        preference = 'weekday_full_day'
+    else:
+        preference = ''
+
+    if preference == 'fees':
+        newList = sorted(list1, key=lambda x: float(x[preference]))
+    else:
+        newList = sorted(list1, key=lambda x: x[preference])
+    
+    return newList
